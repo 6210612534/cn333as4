be9ba5f5698e5d4279d933d033d13948368517b3
Thanpisit Kaemthong <6210612534@student.tu.ac.th>
2022-04-20 14:28:44 +0700

    Complete

---
+++ app/src/main/java/com/example/unitconverter/viewmodels/WeightViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.unitconverter.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.unitconverter.R
+import java.lang.NumberFormatException
+
+class WeightViewModel : ViewModel() {
+    private val _scale: MutableLiveData<Int> = MutableLiveData(R.string.kilogram)
+
+    val scale: LiveData<Int>
+        get() = _scale
+
+    fun setScale(value: Int) {
+        _scale.value = value
+    }
+
+    private val _weight: MutableLiveData<String> = MutableLiveData("")
+
+    val weight: LiveData<String>
+        get() = _weight
+
+    fun getWeightAsFloat(): Float = (_weight.value ?: "").let {
+        return try {
+            it.toFloat()
+        } catch (e: NumberFormatException) {
+            Float.NaN
+        }
+    }
+
+    fun setWeight(value: String) {
+        _weight.value = value
+    }
+
+    fun convert() = getWeightAsFloat().let {
+        if (!it.isNaN())
+            if (_scale.value == R.string.kilogram)
+                (it * 2.205F)
+            else
+                (it / 2.205F)
+        else
+            Float.NaN
+    }
+}
